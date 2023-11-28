40e72c37c0ed1c240437eaf1985a9d0a4f0eac7e
saraelbana <matika20@gmail.com>
2023-11-28 16:46:49 +0100

    First commit

--- src/nl/novi/opdrachten/beslissingsstructuren/Geslacht.java
+++ src/nl/novi/opdrachten/beslissingsstructuren/Geslacht.java
@@ -8,7 +8,15 @@ public class Geslacht {
 
         System.out.println("Wat is uw geslacht?");
         String geslacht = userInputScanner.nextLine();
-
+        geslacht = geslacht.toLowerCase();
+        if(geslacht.equals("man")){
+            System.out.print("Dag meneer");
+        } else if (geslacht.equals("vrouw")) {
+            System.out.print("Dag mevrouw");
+        }
+        else{
+            System.out.print("Dag persoon");
+        }
         /*
         Hier boven heeft de gebruiker zijn geslacht ingevoerd. Print met behulp van een beslissingsstructuur (if/else)
         de volgende berichten:
